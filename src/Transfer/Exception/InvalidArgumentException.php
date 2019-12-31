cb0e70c756f05720f40f4555b3b4c5ca943a612e
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:51:00 -0600

    Rewriting as Laminas Project package

--- src/Transfer/Exception/InvalidArgumentException.php
+++ src/Transfer/Exception/InvalidArgumentException.php
@@ -1,20 +1,18 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_File
+ * @see       https://github.com/laminas/laminas-file for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-file/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-file/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\File\Transfer\Exception;
+namespace Laminas\File\Transfer\Exception;
 
-use Zend\File\Exception;
+use Laminas\File\Exception;
 
 /**
- * @category   Zend
- * @package    Zend_File_Transfer
+ * @category   Laminas
+ * @package    Laminas_File_Transfer
  */
 class InvalidArgumentException extends Exception\InvalidArgumentException implements
     ExceptionInterface
