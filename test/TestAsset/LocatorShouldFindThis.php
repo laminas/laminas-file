67ec9901affbc7b5381427c0bd5dbce4bd8429bc
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:50:58 -0600

    Rewriting as Laminas Project package

--- test/TestAsset/LocatorShouldFindThis.php
+++ test/TestAsset/LocatorShouldFindThis.php
@@ -1,17 +1,15 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_File
+ * @see       https://github.com/laminas/laminas-file for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-file/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-file/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\File\TestAsset;
+namespace LaminasTest\File\TestAsset;
 
 /**
- * @package    Zend_Loader
+ * @package    Laminas_Loader
  * @subpackage Exception
  */
 interface LocatorShouldFindThis
