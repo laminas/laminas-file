e42b6116269d90f10d1b8d17d659b6bebb8fc252
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:50:58 -0600

    Rewriting as Laminas Project package

--- src/Exception/ExceptionInterface.php
+++ src/Exception/ExceptionInterface.php
@@ -1,19 +1,17 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_File
+ * @see       https://github.com/laminas/laminas-file for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-file/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-file/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\File\Exception;
+namespace Laminas\File\Exception;
 
 /**
  * Marker interface for exceptions found in this component
  *
- * @package    Zend_File
+ * @package    Laminas_File
  * @subpackage Exception
  */
 interface ExceptionInterface
--- src/Transfer/Adapter/Http.php
+++ src/Transfer/Adapter/Http.php
@@ -1,25 +1,23 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_File
+ * @see       https://github.com/laminas/laminas-file for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-file/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-file/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\File\Transfer\Adapter;
+namespace Laminas\File\Transfer\Adapter;
 
-use Zend\File\Transfer;
-use Zend\File\Transfer\Exception;
-use Zend\ProgressBar;
-use Zend\ProgressBar\Adapter;
+use Laminas\File\Transfer;
+use Laminas\File\Transfer\Exception;
+use Laminas\ProgressBar;
+use Laminas\ProgressBar\Adapter;
 
 /**
  * File transfer adapter class for the HTTP protocol
  *
- * @category  Zend
- * @package   Zend_File_Transfer
+ * @category  Laminas
+ * @package   Laminas_File_Transfer
  */
 class Http extends AbstractAdapter
 {
@@ -121,7 +119,7 @@ class Http extends AbstractAdapter
             $temp = array($files => array(
                 'name'  => $files,
                 'error' => 1));
-            $validator = $this->validators['Zend\Validator\File\Upload'];
+            $validator = $this->validators['Laminas\Validator\File\Upload'];
             $validator->setTranslator($this->getTranslator())
                       ->setFiles($temp)
                       ->isValid($files, null);
@@ -295,7 +293,7 @@ class Http extends AbstractAdapter
             throw new Exception\PhpEnvironmentException('Neither APC nor UploadProgress extension installed');
         }
 
-        $session = 'Zend\File\Transfer\Adapter\Http\ProgressBar';
+        $session = 'Laminas\File\Transfer\Adapter\Http\ProgressBar';
         $status  = array(
             'total'    => 0,
             'current'  => 0,
