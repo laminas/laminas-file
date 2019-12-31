62a00d1677079d6ca69586aaa8105f490fec1ad8
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:51:13 -0600

    Rewriting as Laminas Project package

--- test/Transfer/Adapter/AbstractAdapterTestMockAdapter.php
+++ test/Transfer/Adapter/AbstractAdapterTestMockAdapter.php
@@ -1,20 +1,19 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-file for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-file/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-file/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\File\Transfer\Adapter;
+namespace LaminasTest\File\Transfer\Adapter;
 
-use Zend\File\Transfer\Adapter;
+use Laminas\File\Transfer\Adapter;
 
 /**
- * Test class for Zend\File\Transfer\Adapter\AbstractAdapter
+ * Test class for Laminas\File\Transfer\Adapter\AbstractAdapter
  *
- * @group      Zend_File
+ * @group      Laminas_File
  */
 class AbstractAdapterTestMockAdapter extends Adapter\AbstractAdapter
 {
