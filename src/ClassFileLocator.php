863e865a2fa92a5018670f6903a305841a199528
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:06:46 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/ClassFileLocator.php
+++ src/ClassFileLocator.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas/laminas-file for the canonical source repository
- * @copyright https://github.com/laminas/laminas-file/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-file/blob/master/LICENSE.md New BSD License
- */
-
 namespace Laminas\File;
 
 use DirectoryIterator;
